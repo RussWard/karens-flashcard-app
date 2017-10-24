32adba1cfdfe8da8b2b016974c76bf00a545181f
Russ Ward <maddogward@gmail.com>
2017-10-24 10:21:58 -0700

    Change mongodb uri to .env variable for deploy

--- server/index.js
+++ server/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const routes = require('./routes');
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
-// const url = 'mongodb://localhost:27017/karens-flashcard-app';
 
 require('dotenv').config();
 
@@ -20,28 +17,7 @@ app.use('/webster', routes.webster);
 app.use('/flashcards', routes.flashcards);
 
 
-// const insertDocuments = (db, callback) => {
-//   let collection = db.collection('documents');
-//   collection.insertMany([
-//     {a : 1}, {a : 2}, {a : 3}
-//   ], (err, result) =>{
-//     assert.equal(err, null);
-//     assert.equal(3, result.result.n);
-//     assert.equal(3, result.ops.length);
-//     console.log('inserted 3 documents into the collection');
-//     callback(result);
-//   })
-// }
 
-// const findDocuments = (db, callback) => {
-//   let collection = db.collection('documents');
-//   collection.find({}).toArray((err, docs) => {
-//     assert.equal(err, null);
-//     console.log('found the following records');
-//     console.log(docs);
-//     callback(docs);
-//   })
-// }
 
 // const dropCollection = (db, callback) => {
 //   let collection = db.collection('documents');
--- server/models/flashcards.js
+++ server/models/flashcards.js
@@ -1,18 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const url = 'mongodb://localhost:27017/karens-flashcard-app';
-
-// const insert = (flashcard, db, callback) => {
-//   let collection = db.collection('documents');
-//   collection.insertOne(flashcard, (err, result) =>{
-//     if (err) {
-//       console.log('Error inserting document ', err);
-//     } else {
-//       console.log('Document inserted ');
-//       callback(result);
-//     }
-//   })
-// }
+const url = process.env.MONGODB_URI;
 
 module.exports.getFlashcards = (cb) => {
   MongoClient.connect(url, (err, db) => {
@@ -45,31 +33,6 @@ module.exports.insertFlashcard = (card, cb) => {
           cb(null, result);
         }
       })
-      // insert(card, db, () => {
-      //   db.close();
-      // })
     }
   })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// MongoClient.connect(url, (err, db) => {
-//   assert.equal(null, err);
-//   console.log('Connected successfully to server');
-//   findDocuments(db, () => {
-//     dropCollection(db, () => {
-//       db.close();
-//     })
-//   })
-// });
